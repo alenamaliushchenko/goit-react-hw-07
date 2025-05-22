c02d3146c6a0d65acb687dbcabc44f9c909cf8bc
Alena Maliushchenko <alenamaliushchenko@gmail.com>
2025-05-22 08:37:39 +0300

    changed mistake

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,18 +1,14 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { deleteContact } from '../../redux/contactsOps';
+import { selectFilteredContacts } from '../../redux/contactsSlice';
 import Contact from './Contact/Contact';
 import css from './ContactList.module.css';
 
 
 const ContactList = () => {
     const dispatch = useDispatch();
-    const contacts = useSelector(state => state.contacts?.items || []);
-    const filter = useSelector(state => state.filters?.name || "");
 
-    const visibleContacts = contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase())
-);
-    
-    
+    const visibleContacts = useSelector(selectFilteredContacts);    
     return (
         <ul className={css.contactList}>
             {visibleContacts.map (contact => (
